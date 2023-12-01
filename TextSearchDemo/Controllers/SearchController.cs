e5d177844920c9687b601d565c5ee2c8c14d980c
Roman Ponomarchuk <roman.ponomarchuk@gmail.com>
2023-12-01 15:19:12 +0100

    Add project files.

---
+++ TextSearchDemo/Controllers/SearchController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TextSearchDemo.Controllers
+{
+    public class SearchController : ControllerBase
+    {
+        private readonly ILogger<SearchController> _logger;
+
+        public SearchController(ILogger<SearchController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Search(string text, CancellationToken cancellationToken)
+        {
+            return null;
+        }
+    }
+}
